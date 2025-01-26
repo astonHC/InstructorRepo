785dfa52e9a220c5e73950eb6a45eb2d4ff1457f
Harry Clark <139020900+astonHC@users.noreply.github.com>
2025-01-26 13:16:54 +0000

    might have gone a bit ott

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -1,23 +1,35 @@
-//Lab01 Java file
-// Sample Java file
-void testMethod1()
-  {
-  //First command x = y + z
-  //Second commnad print(x)
+// COPYRIGHT (C) HARRY CLARK 2025
 
+// LAB 1 FILL-IN
 
-  //Last line print('Good bye')
-}
+// NOT MUCH TO POINT OUT OTHER THAN THAT THIS LAB IS JUST A MEANS OF GETTING TO GRIPS WITH GITHUB
 
-void testMethod2()
-  {
-  // First command x = //to be filled in by student
-  // Second command 
-  // Third command
-}
+public final class LAB_01
+{
+    private static final int a = 3;
+    private static final int b = 7;
+    private static final int y = 10;
+    private static final int z = 5;
 
-void main()
-  {
-    testMethod1();
-    testMethod2();
+    public final static void TEST_METHOD_1()
+    {
+        final int x = y + z;
+        System.out.println(x); 
+
+        System.out.println("Goodbye");
+    }
+
+    public final static void TEST_METHOD_2()
+    {
+        final int x = a * b;
+        final int y = x + 5;
+
+        System.out.println(y);
+    }
+
+    public final static void LAB_01_MAIN(String[] args)
+    {
+        TEST_METHOD_1();
+        TEST_METHOD_2();
+    }
 }
